0e61db84eaa981f1e6be020a2c814b376b21c46e
TaylorTayTyT <taylornguyen904@gmail.com>
2022-05-13 16:02:39 -0400

    testing things out

--- src/App.js
+++ src/App.js
@@ -2,7 +2,25 @@
 import "./App.css"
 import Card from "./Components/Card";
 import Board from "./Components/Board";
+import { styled } from "@mui/material/styles";
+import Paper from "@mui/material/Paper";
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import { Container } from "@material-ui/core";
+import Grid from '@mui/material/Grid';
+
 const canvas = document.getElementById("canv");
+
+const Item = styled(Paper)(({ theme }) => (
+  {
+    backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: "center",
+    color: theme.palette.text.secondary
+  }
+)
+);
 
 function App() {
   return (
@@ -10,11 +28,44 @@ function App() {
       <div className="headerTitle">
         <h1>Tieng Len</h1>
       </div>
-      <div className = "card">
+      <div className="card">
         <Card
-          ctx = {canvas}
+          ctx={canvas}
         />
       </div>
+      <Container>
+        <Button variant="contained">Hello World</Button>
+        <Box
+          sx={{
+            width: 300,
+            height: 300,
+            backgroundColor: 'primary.dark',
+            '&:hover': {
+              backgroundColor: 'primary.main',
+              opacity: [0.9, 0.8, 0.7],
+            },
+          }}
+        />
+
+        <Box display="grid" gridTemplateColumns="repeat(12, 1fr)" gap={10}>
+          <Box gridColumn="span 8">
+            <Item>
+              <Button variant="text">
+                Example
+              </Button>
+            </Item>
+          </Box>
+          <Box gridColumn="span 4">
+            <Item>xs=4</Item>
+          </Box>
+          <Box gridColumn="span 4">
+            <Item>xs=4</Item>
+          </Box>
+          <Box gridColumn="span 8">
+            <Item>xs=8</Item>
+          </Box>
+        </Box>
+      </Container>
     </div>
   )
 
