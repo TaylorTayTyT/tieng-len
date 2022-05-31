2f4113b67e9850f03b8d19c3f16ba0422a5a53c8
Taylor Nguyen <taylornguyen904@gmail.com>
2022-05-30 20:22:28 -0700

    idt you can use nunjucks and react together...

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const router = express.Router(); 
+const router = express.Router();
 
 router.get("/", (req, res) => {
     res.render("../views/home.njk");
@@ -7,6 +7,31 @@ router.get("/", (req, res) => {
 
 router.get("/new", (req, res) => {
     res.render("../views/home.njk");
+})
+
+router.post("/", (req, res) => {
+    res.send("userCreated")
+})
+
+//:id is a paramater
+
+router
+    .route("/:id").get((req, res) => {
+        res.send(`YPPP ID ${req.params.id}`)
+    })
+    .put((req, res) => {
+        res.send(`YPPP ID ${req.params.id}`)
+    })
+    .delete((req, res) => {
+        res.send(`YPPP ID ${req.params.id}`)
+    })
+
+    const users = [{name: "Kyle"}, {name: "Sally"}]
+
+router.param("id", (req, res, next, id) => {
+    console.log(id);
+    req.user = users[id];
+    next();
 })
 
 module.exports = router
--- server.js
+++ server.js
@@ -8,13 +8,23 @@ nunjucks.configure(__dirname + "/views", {
     express: app
 });
 
+//renders html files in public
+//app.use(express.static("public"));
+
+//lets you access middleware
+app.use(express.urlencoded({extended: true}));
+
+//lets you parse json info
+app.use(express.json());
+
 app.get("/", (req, res) => {
+    console.log("hi");
     res.render("home.njk", {t: "Taylor"})
 })
 
 const userRouter = require("./routes/users");
 
-
+//redirects tp the routes in ./routes/users files
 app.use("/users", userRouter);
 
 app.listen(3000);
